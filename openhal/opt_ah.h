5825bb0a567cd55a2260516614d96a1397ffb777
proski <proski@0192ed92-7a03-0410-a25b-9323aeb14dbd>
2007-02-11 02:25:37 +0000

    Typo: "regular" should be "regulatory"
    git-svn-id: http://madwifi-project.org/svn/branches/madwifi-old-openhal@2102 0192ed92-7a03-0410-a25b-9323aeb14dbd

--- openhal/opt_ah.h
+++ openhal/opt_ah.h
@@ -1,4 +1,4 @@
-/* Set this to 1 to disable regular domain restrictions */
+/* Set this to 1 to disable regulatory domain restrictions */
 #define SUPERCHANNEL    0
 
 /* Uncomment this for debuging (warning that results TOO much output) */
