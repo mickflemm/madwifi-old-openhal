b7969cd36c8a58cc2fb943b90abd706e72878899
proski <proski@0192ed92-7a03-0410-a25b-9323aeb14dbd>
2007-07-03 21:44:11 +0000

    Copy from trunk
    git-svn-id: http://madwifi-project.org/svn/branches/madwifi-old-openhal@2532 0192ed92-7a03-0410-a25b-9323aeb14dbd

--- scripts/madwifi-unload.bash
+++ scripts/madwifi-unload.bash
@@ -1,7 +1,52 @@
 #!/bin/bash
 
-for module in ath_{pci,rate_{amrr,minstrel,onoe,sample}} \
-	      wlan_{wep,tkip,ccmp,acl,xauth,scan_{sta,ap}} ath_hal wlan
-do
-	grep -q ^$module /proc/modules && modprobe -r $module || true
+PATTERN='^\(ath_.*\|wlan_.*\|wlan\)$'
+
+MAX_TRIES=10
+
+fatal()
+{
+	echo "FATAL: $1" >&2
+	exit 1
+}
+
+[ "$UID" = 0 ] || fatal "You must be root to run this script"
+[ -r /proc/modules ] || fatal "Cannot read /proc/modules"
+
+tries="$MAX_TRIES"
+while [ "$tries" != "0" ]; do
+	skipped=0
+	cat /proc/modules | while true; do
+		read -r name size use_count use_name state trailer || \
+			exit "$skipped"
+
+		expr "$name" : "$PATTERN" >/dev/null || continue
+
+		# Compatibility for Linux 2.4.x
+		[ "$state" = "" ] && { use_name="-"; state="Live"; }
+
+		if [ "$state" != "Live" ] || [ "$use_count" != "0" ] || \
+		   [ "$use_name" != "-" ]; then
+			skipped=1
+			if [ "$tries" = "1" ]; then
+				echo "Cannot unload \"$name\"" >&2
+			fi
+			continue
+		fi
+
+		echo "Unloading \"$name\""
+		sync	# to be safe
+		/sbin/rmmod "$name" || fatal "cannot unload module \"$name\""
+		sync    # to be even safer
+	done
+	skipped="$?"
+	[ "$skipped" = "0"  ] && break
+	tries=$(($tries - 1))
 done
+
+if [ "$skipped" = "1" ]; then
+	echo "Error - some modules could not be unloaded" >&2
+	exit 1
+fi
+
+exit 0
